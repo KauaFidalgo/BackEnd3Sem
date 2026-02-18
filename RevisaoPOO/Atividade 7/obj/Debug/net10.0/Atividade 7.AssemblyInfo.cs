89dea26cd12661213bfca6419770adfd81c18846
KAUA FIDALGO DROPPA GARCIA <55435579813@info.local>
2026-02-18 09:12:02 -0300

    FrontEnd Realizado

--- RevisaoPOO/Atividade 7/obj/Debug/net10.0/Atividade 7.AssemblyInfo.cs
+++ RevisaoPOO/Atividade 7/obj/Debug/net10.0/Atividade 7.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Atividade 7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+833175105cf97c488109425b9591cd8f556b00d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc0b9ab8ba5f80be451ce068e0a2b4d2beb73972")]
 [assembly: System.Reflection.AssemblyProductAttribute("Atividade 7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Atividade 7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
